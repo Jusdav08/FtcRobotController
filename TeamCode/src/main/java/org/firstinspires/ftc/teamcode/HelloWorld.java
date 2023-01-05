76a4170b883affb9f51cb15d44cc604597e12e80
Jusdav08 <121832442+Jusdav08@users.noreply.github.com>
2023-01-05 16:20:05 -0500

    Update HelloWorld.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -1,13 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp()
+@Autonomous() //this determines which category (auto or teleop) that this code falls under
 public class HelloWorld extends OpMode {
     @Override
     public void init() {
-        telemetry.addData("Hello", "Justin");
+        String myName = "Justin Davis";
+        telemetry.addData("Hello", "myName");// this string will display on the driver station as "caption: value"
     }
 
     @Override
@@ -15,4 +17,5 @@ public class HelloWorld extends OpMode {
 
     }
 }
+
 
